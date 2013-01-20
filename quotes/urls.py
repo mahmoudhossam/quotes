4a9267dfed3cc96ab0c0d53ff20b2319ae01e7ac
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2013-01-20 17:22:00 +0200

    Add new quote.

--- quotes/urls.py
+++ quotes/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'quotes.views.home', name='home'),
     # url(r'^quotes/', include('quotes.foo.urls')),
+    url(r'^$', 'webapp.views.add_quote'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- webapp/views.py
+++ webapp/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.template.response import TemplateResponse
+from django.views.decorators.http import require_http_methods
+from django.shortcuts import redirect
+from models import *
+
+@require_http_methods(['GET', 'POST'])
+def add_quote(request):
+    if request.method == 'GET':
+        return TemplateResponse(request, 'new.html')
+    else:
+        text = request.POST.get('quote')
+        src = request.POST.get('source')
+        quote = Quote(quote_text=text, quote_source=src)
+        quote.save()
