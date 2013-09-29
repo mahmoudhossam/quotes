9df6ff13c99c958dca8df713fce59383fbdefad4
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2013-09-29 03:21:06 +0200

    Each quote object is now tied to a creator

--- webapp/forms.py
+++ webapp/forms.py
@@ -5,4 +5,4 @@ from .models import Quote
 class AddForm(forms.ModelForm):
     class Meta:
         model = Quote
-        exclude = ('added_on',)
+        exclude = ('added_on', 'added_by')
--- webapp/models.py
+++ webapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Quote(models.Model):
     quote_text = models.TextField()
     quote_source = models.CharField(max_length=100)
     added_on = models.DateTimeField(auto_now_add=True)
+    added_by = models.ForeignKey(User, default=1)
