f8da436153d76d62b407b3aa72c9271292c1a4b8
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2013-11-30 16:06:31 +0200

    dj-static sucks

--- quotes/settings.py
+++ quotes/settings.py
@@ -4,6 +4,8 @@ import os
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 ADMINS = (
     ('Mahmoud Hossam', os.getenv('ADMIN_EMAIL')),
 )
@@ -48,7 +50,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
--- quotes/urls.py
+++ quotes/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
+from django.conf import settings
 
 
 admin.autodiscover()
@@ -11,4 +12,6 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     (r'^accounts/', include('allauth.urls')),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+     {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
 )
